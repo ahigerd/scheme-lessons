f4b6143d04c5bb42e6c61260db5d320919eef595
Adam Higerd <chighland@gmail.com>
2020-07-08 14:59:28 -0500

    add typechecks to propget

--- struct.scm
+++ struct.scm
@@ -6,14 +6,26 @@
   [(-symbol-alist ?n ?a . ?b) (cons (list '?a ?n) (-symbol-alist (+ ?n 1) . ?b))]
   [(-symbol-alist ?n) '()])
 
-(define (-define-propgetx name prop index)
-  (list 'define (list (string->symbol (string-append (symbol->string name) "-" (symbol->string prop))) 'x) (list 'list-ref 'x index))
+(define (-define-propgetx struct prop index)
+  (let* [
+    (symbol-append (lambda (parts) (string->symbol (apply string-append parts))))
+    (structname (symbol->string struct))
+    (propname (symbol->string prop))
+    (struct? (symbol-append (list structname "?")))
+    (struct-prop (symbol-append (list structname "-" propname)))
+  ]
+  (list 'define (list struct-prop 'x)
+    (list 'if (list struct? 'x)
+      (list 'list-ref 'x index)
+      (list 'error (symbol->string struct-prop) "expects ~a, given ~a" structname 'x)
+    )
+  )
 )
 
 (define-syntax define-struct
   [(define-struct ?name ?props) (begin
      (eval (list 'define (-concat-name make- ?name) '(lambda ?props (list '?name . ?props))))
-     (eval (list 'define (-concat-name ?name ?) '(lambda (x) (and (eq? (car x) '?name) (= (length '?props) (length (cdr x)))))))
+     (eval (list 'define (-concat-name ?name ?) '(lambda (x) (and (pair? x) (eq? (car x) '?name) (= (length '?props) (length (cdr x)))))))
      (for-each (lambda (p)
        (eval (apply (lambda (prop x) (-define-propgetx '?name prop x)) p))
      ) (-symbol-alist 1 . ?props))
